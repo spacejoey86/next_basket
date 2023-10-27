b1ab93fe25684dcf9a30104618526c33151f474a
vcokltfre <vcokltfre@gmail.com>
2023-10-27 17:49:43 +0100

    fix: uncomment important code

--- Main.java
+++ Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) {
         ShoppingBag myShoppingBag = new ShoppingBag();
 
-        // myShoppingBag.addItem(new Item("ABC010"));
-        // myShoppingBag.addItem(new Item("ABC010"));
-        // myShoppingBag.addItem(new Item("DEF020"));
+        myShoppingBag.addItem(new Item("ABC010"));
+        myShoppingBag.addItem(new Item("ABC010"));
+        myShoppingBag.addItem(new Item("DEF020"));
 
         myShoppingBag.addDiscount(DiscountFactory.createDiscount("ABC2P50"));
         myShoppingBag.addDiscount(DiscountFactory.createDiscount("DEF1C05"));
